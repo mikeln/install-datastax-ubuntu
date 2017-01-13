eca11153bd192adaa469501a73f7936c72694d06
Mikel Nelson <github@mikelnelson.net>
2017-01-12 17:09:26 -0800

    add 2nd pw

--- bin/dse/configure_agent_address_yaml.sh
+++ bin/dse/configure_agent_address_yaml.sh
@@ -6,6 +6,8 @@ opscenter_ip=$3
 # security
 cass_user=${4:-cassandra}
 cass_pass=${5:-cassandra}
+mcass_user=${6:-cassandra}
+mcass_pass=${7:-cassandra}
 
 # looks like the agent creates an empty /var/lib/datastax-agent/conf/address.yaml when it starts for the first time
 # given that, we're not going to worry about backing it up
@@ -18,6 +20,8 @@ agent_rpc_interface: $node_ip
 agent_rpc_broadcast_address: $node_broadcast_ip
 cassandra_user: $cass_user
 cassandra_pass: $cass_pass
+monitored_cassandra_user: $mcass_user
+monitored_cassandra_pass: $mcass_pass
 /EOF
 
 chown cassandra $file
