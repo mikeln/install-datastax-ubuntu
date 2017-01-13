d9240942329ae91c6a80d14cbf4b0b17d81e2b38
Mikel Nelson <github@mikelnelson.net>
2017-01-12 16:56:25 -0800

    bash fix

--- bin/dse/configure_agent_address_yaml.sh
+++ bin/dse/configure_agent_address_yaml.sh
@@ -4,10 +4,8 @@ node_ip=$1
 node_broadcast_ip=$2
 opscenter_ip=$3
 # security
-cass_user=$4
-cass_user="${$cass_user:-cassandra}"
-cass_pass=$5
-cass_pass="${$cass_pass:-cassandra}"
+cass_user=${4:-cassandra}
+cass_pass=${5:-cassandra}
 
 # looks like the agent creates an empty /var/lib/datastax-agent/conf/address.yaml when it starts for the first time
 # given that, we're not going to worry about backing it up
