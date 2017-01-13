c754f9c5332cce7380203cff788218f72c54cf6f
Mikel Nelson <github@mikelnelson.net>
2017-01-12 16:43:36 -0800

    add user:pw not prod ready

--- bin/dse/configure_agent_address_yaml.sh
+++ bin/dse/configure_agent_address_yaml.sh
@@ -3,6 +3,12 @@
 node_ip=$1
 node_broadcast_ip=$2
 opscenter_ip=$3
+# security
+cass_user=$4
+cass_user="${$cass_user:-cassandra}"
+cass_pass=$5
+cass_pass="${$cass_pass:-cassandra}"
+
 # looks like the agent creates an empty /var/lib/datastax-agent/conf/address.yaml when it starts for the first time
 # given that, we're not going to worry about backing it up
 
@@ -12,6 +18,8 @@ cat <</EOF > $file
 stomp_interface: $opscenter_ip
 agent_rpc_interface: $node_ip
 agent_rpc_broadcast_address: $node_broadcast_ip
+cassandra_user: $cass_user
+cassandra_pass: $cass_pass
 /EOF
 
 chown cassandra $file
